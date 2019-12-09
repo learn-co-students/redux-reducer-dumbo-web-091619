2e2fbea298066bdf6153c3068c3be41e2a0d6c6a
EthanJF <ethanjosephfournier@gmail.com>
2019-12-09 13:36:15 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
 export function manageFriends(state, action){
+    switch (action.type){
+        case 'ADD_FRIEND':
+            return {friends: [...state.friends, {name: action["friend"]["name"], hometown: action["friend"]["hometown"], id: action["friend"]["id"]}]}
+        case 'REMOVE_FRIEND':
+            const newFriends = state.friends.filter((eachFriend) => {
+                return eachFriend.id !== action.id
+            })
+            return {friends: newFriends }
+        default: 
+            return state
+    }
 }
+
